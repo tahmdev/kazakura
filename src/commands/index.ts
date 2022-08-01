f26dd5301f87c69407f37af71befa0b123393925
tahmdev <qwertiimusic@gmail.com>
2022-08-02 00:51:27 +0200

    Add manageTags

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,3 +2,4 @@ export * as hello from "./hello";
 export * as clear from "./clear";
 export * as t from "./t";
 export * as echo from "./echo";
+export * as manage_tags from "./manageTags";
---
+++ src/commands/manageTags.ts
@@ -0,0 +1,56 @@
+import {
+  SlashCommandBuilder,
+  CommandInteraction,
+  Client,
+  PermissionFlagsBits,
+  ModalSubmitInteraction,
+} from "discord.js";
+import * as subCommandModules from "./manageTags/index";
+
+export const data = new SlashCommandBuilder()
+  .setName("manage_tags")
+  .setDescription("Add, edit, or delete tags")
+  .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+  .addSubcommand((subcommand) =>
+    subcommand.setName("add").setDescription("Add a tag")
+  )
+  .addSubcommand((subcommand) =>
+    subcommand.setName("edit").setDescription("Edit a tag")
+  )
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName("delete")
+      .setDescription("Delete a tag")
+      .addStringOption((option) =>
+        option
+          .setName("tag")
+          .setDescription("Enter a tag to delete")
+          .setAutocomplete(true)
+          .setRequired(true)
+      )
+  );
+
+export async function autoComplete(
+  interaction: CommandInteraction,
+  client: Client
+) {
+  const subCommands = Object(subCommandModules);
+  const { name } = interaction.options.data[0];
+  subCommands[name].autoComplete(interaction, client);
+}
+
+export async function execute(interaction: CommandInteraction, client: Client) {
+  const subCommands = Object(subCommandModules);
+  const { name } = interaction.options.data[0];
+  subCommands[name].execute(interaction, client);
+}
+
+export async function handleModal(
+  interaction: ModalSubmitInteraction,
+  client: Client
+) {
+  const subCommands = Object(subCommandModules);
+  const { customId } = interaction;
+  const key = customId.split(".")[1];
+  subCommands[key].handleModal(interaction, client);
+}
