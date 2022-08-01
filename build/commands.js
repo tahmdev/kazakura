72e78cecbf9d67f29f7a0771df4a6b71cce952e6
tahmdev <qwertiimusic@gmail.com>
2022-08-01 03:45:07 +0200

    Gitignore build folder

--- build/commands.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Commands = void 0;
-const clear_1 = require("./commands/clear");
-exports.Commands = [clear_1.Clear];
