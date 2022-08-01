aefe16f65218712dd56d57c0aabb37db191436d7
tahmdev <qwertiimusic@gmail.com>
2022-08-01 03:45:57 +0200

    Add autocomplete and modal handling

--- src/listeners/interaction-create.ts
+++ src/listeners/interaction-create.ts
@@ -1,11 +1,27 @@
-import { Client } from "discord.js";
+import { Client, InteractionType } from "discord.js";
 import * as commandModules from "../commands/index";
 
 export default (client: Client) => {
   client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) return;
+    if (
+      !interaction.isCommand() &&
+      !interaction.isAutocomplete() &&
+      !interaction.isModalSubmit()
+    )
+      return;
     const commands = Object(commandModules);
-    const { commandName } = interaction;
-    commands[commandName].execute(interaction, client);
+
+    if (interaction.type === InteractionType.ModalSubmit) {
+      const { customId } = interaction;
+      commands[customId].handleModal(interaction, client);
+    } else if (
+      interaction.type === InteractionType.ApplicationCommandAutocomplete
+    ) {
+      const { commandName } = interaction;
+      commands[commandName].autoComplete(interaction, client);
+    } else {
+      const { commandName } = interaction;
+      commands[commandName].execute(interaction, client);
+    }
   });
 };
