d861d4b97d54332a8f6cc2a1b50835f528044a65
tahmdev <qwertiimusic@gmail.com>
2022-08-01 03:45:23 +0200

    Add permission to clear

--- src/commands/clear.ts
+++ src/commands/clear.ts
@@ -3,13 +3,15 @@ import {
   CommandInteraction,
   Client,
   TextChannel,
+  PermissionFlagsBits,
 } from "discord.js";
 export const data = new SlashCommandBuilder()
   .setName("clear")
   .setDescription("Clear the last N messages")
   .addIntegerOption((option) =>
     option.setName("amount").setDescription("Amount of messags to delete")
-  );
+  )
+  .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels);
 export async function execute(interaction: CommandInteraction, client: Client) {
   const n = interaction.options.get("amount")?.value || 5;
   const channel = interaction.channel as TextChannel;
