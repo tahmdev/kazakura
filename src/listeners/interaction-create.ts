29d2089416fe78aab0492f21f4d268c043d17429
tahmdev <qwertiimusic@gmail.com>
2022-08-11 10:34:00 +0200

    Add permission logic

--- src/listeners/interaction-create.ts
+++ src/listeners/interaction-create.ts
@@ -1,4 +1,5 @@
-import { Client, InteractionType } from "discord.js";
+import { Client, GuildMemberRoleManager, InteractionType } from "discord.js";
+import { cache } from "../cache/cache";
 import * as commandModules from "../commands/index";
 
 export default (client: Client) => {
@@ -9,6 +10,7 @@ export default (client: Client) => {
       !interaction.isModalSubmit()
     )
       return;
+
     const commands = Object(commandModules);
 
     if (interaction.type === InteractionType.ModalSubmit) {
@@ -22,7 +24,38 @@ export default (client: Client) => {
       commands[commandName].autoComplete(interaction, client);
     } else {
       const { commandName } = interaction;
-      commands[commandName].execute(interaction, client);
+
+      if (!interaction.channel) {
+        // Will be null if it's a DM
+        return commands[commandName].execute(interaction, client);
+      }
+
+      const { guildId } = interaction;
+      if (!guildId)
+        return interaction.reply({
+          content: "Something went wrong",
+          ephemeral: true,
+        });
+
+      const permissions = cache.guild(guildId).permissions[commandName];
+      const roles = (interaction.member?.roles as GuildMemberRoleManager).cache;
+
+      if (!permissions) {
+        return interaction.reply({
+          content: "This command is not available on this server.",
+          ephemeral: true,
+        });
+      }
+
+      if (!roles.some((el) => permissions.includes(el.id))) {
+        return interaction.reply({
+          content:
+            "You don't have the required permissions to run this command.",
+          ephemeral: true,
+        });
+      }
+
+      return commands[commandName].execute(interaction, client);
     }
   });
 };
